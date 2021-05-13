2bd3d7c95a24eb5f60b4e513f57a03541f36c802
schriker <janekmachine@gmail.com>
2021-05-13 14:00:46 +0200

    Database configuration added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: ['./**/*.entity{.ts,.js}'],
+        migrationsTableName: 'custom_migration_table',
+        migrations: ['./migration/*{.ts,.js}'],
+        cli: {
+          migrationsDir: '../migration',
+        },
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
