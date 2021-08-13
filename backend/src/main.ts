cde91cfbb134b399d1edbabde2d054f82f27c5cc
Marcin Janus <janekmachine@gmail.com>
2021-08-13 12:51:56 +0200

    Trust proxy set

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,13 @@ import * as connectRedis from 'connect-redis';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 let redisClient: Redis.Redis;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 'loopback');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const configService = app.get(ConfigService);
   const RedisStore = connectRedis(session);
@@ -29,7 +31,6 @@ async function bootstrap() {
         disableTouch: true,
         disableTTL: true,
       }),
-      proxy: true,
       cookie: {
         httpOnly: true,
         sameSite: false,
